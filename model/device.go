cf8b41a0b6d695379dc7f1d25b21fe5dbddaf262
王锐彬 <492960423@qq.com>
2019-10-04 01:27:59 +0800

    更新设备表

--- model/device.go
+++ model/device.go
@@ -4,19 +4,6 @@ import (
 	"time"
 )
 
-// UserInfo 用户表
-type UserInfo struct {
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt *time.Time
-	UID       string `gorm:"primary_key"`
-	Username  string
-	Nickname  string
-	Password  string
-	Mobile    string
-	Devices   []*Device `gorm:"many2many:user_devices;"`
-}
-
 // Device 设备表
 type Device struct {
 	CreatedAt time.Time
@@ -24,5 +11,4 @@ type Device struct {
 	DeletedAt *time.Time
 	Sno       string `gorm:"primary_key"`
 	Mac       string
-	UserInfo  []*UserInfo `gorm:"many2many:user_devices;"`
 }
